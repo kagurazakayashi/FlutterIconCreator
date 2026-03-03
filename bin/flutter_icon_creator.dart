6eee34aa1cccc70ca9dabfdefc3a8b7dc99b2f4e
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2026-03-03 20:15:44 +0800

    新建仓库

---
+++ bin/flutter_icon_creator.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_icon_creator/flutter_icon_creator.dart' as flutter_icon_creator;
+
+void main(List<String> arguments) {
+  flutter_icon_creator.run(arguments);
+}
---
+++ lib/flutter_icon_creator.dart
@@ -0,0 +1,4 @@
+void run(List<String> arguments) {
+  print(
+      'Flutter Icon Creator - Command-line tool for generating icons for Flutter projects');
+}
